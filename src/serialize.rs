c67c4387f264530ce366a7913930f4544c7fc82d
Paul Busby <paul.busby@foxpaw.rs>
2024-05-01 21:24:16 +1000

    Primitive Types + Support for serialization of i8, i16, i32, i64, i128, and isize types.

--- src/serialize.rs
+++ src/serialize.rs
@@ -40,6 +40,78 @@ impl Serialize for bool {
     }
 }
 
+impl Serialize for i8 {
+    /// Accept a serializer, allowing it to serialize this item. Note that this is
+    /// an internal method used to serialize from the Serializer and is uncommon to
+    /// use outside this library.
+    fn accept<S>(&self, serializer: &S) -> S::Output
+    where
+        S: Serializer,
+    {
+        serializer.visit_i8(self)
+    }
+}
+
+impl Serialize for i16 {
+    /// Accept a serializer, allowing it to serialize this item. Note that this is
+    /// an internal method used to serialize from the Serializer and is uncommon to
+    /// use outside this library.
+    fn accept<S>(&self, serializer: &S) -> S::Output
+    where
+        S: Serializer,
+    {
+        serializer.visit_i16(self)
+    }
+}
+
+impl Serialize for i32 {
+    /// Accept a serializer, allowing it to serialize this item. Note that this is
+    /// an internal method used to serialize from the Serializer and is uncommon to
+    /// use outside this library.
+    fn accept<S>(&self, serializer: &S) -> S::Output
+    where
+        S: Serializer,
+    {
+        serializer.visit_i32(self)
+    }
+}
+
+impl Serialize for i64 {
+    /// Accept a serializer, allowing it to serialize this item. Note that this is
+    /// an internal method used to serialize from the Serializer and is uncommon to
+    /// use outside this library.
+    fn accept<S>(&self, serializer: &S) -> S::Output
+    where
+        S: Serializer,
+    {
+        serializer.visit_i64(self)
+    }
+}
+
+impl Serialize for i128 {
+    /// Accept a serializer, allowing it to serialize this item. Note that this is
+    /// an internal method used to serialize from the Serializer and is uncommon to
+    /// use outside this library.
+    fn accept<S>(&self, serializer: &S) -> S::Output
+    where
+        S: Serializer,
+    {
+        serializer.visit_i128(self)
+    }
+}
+
+impl Serialize for isize {
+    /// Accept a serializer, allowing it to serialize this item. Note that this is
+    /// an internal method used to serialize from the Serializer and is uncommon to
+    /// use outside this library.
+    fn accept<S>(&self, serializer: &S) -> S::Output
+    where
+        S: Serializer,
+    {
+        serializer.visit_isize(self)
+    }
+}
+
 /// Trait to implement on an item that conducts the serialization, and defines
 /// how data is serialized. Interaction with this should be done using the
 /// serialize method, which in turn calls the required visit methods to
@@ -55,6 +127,24 @@ pub trait Serializer {
 
     /// Visit and serialize a bool type.
     fn visit_bool(&self, input: &bool) -> Self::Output;
+
+    /// Visit and serialize an i8 type.
+    fn visit_i8(&self, input: &i8) -> Self::Output;
+
+    /// Visit and serialize an i16 type.
+    fn visit_i16(&self, input: &i16) -> Self::Output;
+
+    /// Visit and serialize an i32 type.
+    fn visit_i32(&self, input: &i32) -> Self::Output;
+
+    /// Visit and serialize an i64 type.
+    fn visit_i64(&self, input: &i64) -> Self::Output;
+
+    /// Visit and serialize an i128 type.
+    fn visit_i128(&self, input: &i128) -> Self::Output;
+
+    /// Visit and serialize an isize type.
+    fn visit_isize(&self, input: &isize) -> Self::Output;
 
     /// Visit and serialize a unit type.
     fn visit_unit(&self) -> Self::Output;
--- src/serialize/json.rs
+++ src/serialize/json.rs
@@ -53,6 +53,84 @@ impl Serializer for Json {
         input.to_string()
     }
 
+    /// Visit and serialize an i8 type.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use shallot::serialize::{Json, Serializer};
+    ///
+    /// let json = Json::new();
+    /// let output = json.serialize(&true);
+    /// ```
+    fn visit_i8(&self, input: &i8) -> Self::Output {
+        input.to_string()
+    }
+
+    /// Visit and serialize an i16 type.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use shallot::serialize::{Json, Serializer};
+    ///
+    /// let json = Json::new();
+    /// let output = json.serialize(&true);
+    /// ```
+    fn visit_i16(&self, input: &i16) -> Self::Output {
+        input.to_string()
+    }
+
+    /// Visit and serialize an i32 type.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use shallot::serialize::{Json, Serializer};
+    ///
+    /// let json = Json::new();
+    /// let output = json.serialize(&true);
+    /// ```
+    fn visit_i32(&self, input: &i32) -> Self::Output {
+        input.to_string()
+    }
+
+    /// Visit and serialize an i64 type.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use shallot::serialize::{Json, Serializer};
+    ///
+    /// let json = Json::new();
+    /// let output = json.serialize(&true);
+    /// ```
+    fn visit_i64(&self, input: &i64) -> Self::Output {
+        input.to_string()
+    }
+
+    /// Visit and serialize an i128 type.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use shallot::serialize::{Json, Serializer};
+    ///
+    /// let json = Json::new();
+    /// let output = json.serialize(&true);
+    /// ```
+    fn visit_i128(&self, input: &i128) -> Self::Output {
+        input.to_string()
+    }
+
+    /// Visit and serialize an isize type.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use shallot::serialize::{Json, Serializer};
+    ///
+    /// let json = Json::new();
+    /// let output = json.serialize(&true);
+    /// ```
+    fn visit_isize(&self, input: &isize) -> Self::Output {
+        input.to_string()
+    }
+
     /// Visit and serialize a unit type.
     ///
     /// # Examples
@@ -79,17 +157,6 @@ mod tests {
         assert_eq!(expected, actual);
     }
 
-    /// Test Json::visit_unit correctly serializes a unit type.
-    #[test]
-    fn visit_unit_correct() {
-        let expected = "null".to_owned();
-        let actual = Json::new().visit_unit();
-        assert_eq!(expected, actual);
-
-        let actual = Json::new().serialize(&());
-        assert_eq!(expected, actual);
-    }
-
     /// Test Json::visit_bool correctly serializes a true bool type.
     #[test]
     fn visit_bool_true() {
@@ -109,6 +176,83 @@ mod tests {
         assert_eq!(expected, actual);
 
         let actual = Json::new().serialize(&false);
+        assert_eq!(expected, actual);
+    }
+
+    /// Test Json::visit_i8 correctly serializes a i8 type.
+    #[test]
+    fn visit_i8_correct() {
+        let expected = "1".to_owned();
+        let actual = Json::new().visit_i8(&1_i8);
+        assert_eq!(expected, actual);
+
+        let actual = Json::new().serialize(&1_i8);
+        assert_eq!(expected, actual);
+    }
+
+    /// Test Json::visit_i16 correctly serializes a i16 type.
+    #[test]
+    fn visit_i16_correct() {
+        let expected = "1".to_owned();
+        let actual = Json::new().visit_i16(&1_i16);
+        assert_eq!(expected, actual);
+
+        let actual = Json::new().serialize(&1_i16);
+        assert_eq!(expected, actual);
+    }
+
+    /// Test Json::visit_i32 correctly serializes a i32 type.
+    #[test]
+    fn visit_i32_correct() {
+        let expected = "1".to_owned();
+        let actual = Json::new().visit_i32(&1_i32);
+        assert_eq!(expected, actual);
+
+        let actual = Json::new().serialize(&1_i32);
+        assert_eq!(expected, actual);
+    }
+
+    /// Test Json::visit_i64 correctly serializes a i64 type.
+    #[test]
+    fn visit_i64_correct() {
+        let expected = "1".to_owned();
+        let actual = Json::new().visit_i64(&1_i64);
+        assert_eq!(expected, actual);
+
+        let actual = Json::new().serialize(&1_i64);
+        assert_eq!(expected, actual);
+    }
+
+    /// Test Json::visit_i128 correctly serializes a i128 type.
+    #[test]
+    fn visit_i128_correct() {
+        let expected = "1".to_owned();
+        let actual = Json::new().visit_i128(&1_i128);
+        assert_eq!(expected, actual);
+
+        let actual = Json::new().serialize(&1_i128);
+        assert_eq!(expected, actual);
+    }
+
+    /// Test Json::visit_isize correctly serializes a isize type.
+    #[test]
+    fn visit_isize_correct() {
+        let expected = "1".to_owned();
+        let actual = Json::new().visit_isize(&1_isize);
+        assert_eq!(expected, actual);
+
+        let actual = Json::new().serialize(&1_isize);
+        assert_eq!(expected, actual);
+    }
+
+    /// Test Json::visit_unit correctly serializes a unit type.
+    #[test]
+    fn visit_unit_correct() {
+        let expected = "null".to_owned();
+        let actual = Json::new().visit_unit();
+        assert_eq!(expected, actual);
+
+        let actual = Json::new().serialize(&());
         assert_eq!(expected, actual);
     }
 }
